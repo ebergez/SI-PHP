37de07f1d2164184ffa0ab0562eaa43aef8bdeb6
ebergez <emilebergez@gmail.com>
2019-05-28 13:33:14 +0200

    Separated menus and header and added styles

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Resal social</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<?php $pdo = new PDO(
+    'mysql:host=localhost;dbname=SI PHP;',
+    'root',
+    '',
+    [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_WARNING,
+        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+    ]
+);
+session_start();
+$_SESSION["prenom"]= "emile";
+$_SESSION["nom"]= "bergez" 
+?>
+<body>
+<?php include 'menu.php'?>
+<?php include 'header.php'?>
+<?php include 'rightmenu.php'?>
+<section class="test"></section>
+	
+</body>
+<?php session_destroy()?>
+</html>
---
+++ rightmenu.php
@@ -0,0 +1,14 @@
+<section class="rightNav">
+  <div class="rightNavTop">
+    <div class="profilePicAndName">
+      <div class="myProfilePic"></div>
+        <?php echo "<h2 class='myName'>" . $_SESSION["prenom"] . " " . $_SESSION["nom"] . "</h2>"    ?>
+    </div>
+  </div>
+  <ul class="stats">
+    <li></li>
+    <li></li>
+    <li></li>
+
+  </ul>
+</section>
